c2493b8d906be354c5ca211c669b7047e86bc71b
Eloïse Robert <elo.robert41@gmail.com>
2026-08-02 11:14:01 +0200

    modifs vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-// vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // Injection globale pour éviter l'erreur "process is not defined" dans le navigateur
   define: {
+    // Injection globale pour éviter le crash "process is not defined" dans le navigateur
     "process.env": {},
+    process: { env: {} },
   },
   server: {
-    // Empêche Vite de recharger la page quand tu modifies un fichier
     hmr: false,
     watch: {
-      // Optionnel : ignore les fichiers du projet pour éviter le reload
       ignored: ["**/*"],
     },
   },
